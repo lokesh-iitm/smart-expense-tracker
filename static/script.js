17b156c25fd3f9a1654ccb2689993f486f827e4e
local user <local@example.com>
2026-06-15 14:27:24 +0530

    expense form

--- static/script.js
+++ static/script.js
@@ -17,3 +17,74 @@ async function loadDashboard() {
 }
 
 loadDashboard();
+async function loadExpenses() {
+    const response = await fetch("/expenses?limit=100");
+
+    const expenses = await response.json();
+
+    const tableBody =
+        document.getElementById("expense-body");
+
+    tableBody.innerHTML = "";
+
+    expenses.forEach(expense => {
+
+        tableBody.innerHTML += `
+            <tr>
+                <td>${expense.title}</td>
+                <td>${expense.category}</td>
+                <td>${expense.amount}</td>
+                <td>${expense.date}</td>
+            </tr>
+        `;
+
+    });
+}
+
+loadExpenses();
+async function addExpense(event) {
+
+    event.preventDefault();
+
+    const expense = {
+        title:
+            document.getElementById("title").value,
+
+        category:
+            document.getElementById(
+                "expense-category"
+            ).value,
+
+        amount:
+            parseFloat(
+                document.getElementById("amount").value
+            ),
+
+        date:
+            document.getElementById("date").value
+    };
+
+    await fetch("/expenses", {
+        method: "POST",
+        headers: {
+            "Content-Type":
+                "application/json"
+        },
+        body: JSON.stringify(expense)
+    });
+
+    loadDashboard();
+    loadExpenses();
+
+    document.getElementById(
+        "expense-form"
+    ).reset();
+}
+
+
+document
+    .getElementById("expense-form")
+    .addEventListener(
+        "submit",
+        addExpense
+    );
